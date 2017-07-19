a0d96c01d069bb2d6c594e7aeefb2da0f481ebae
Bangda Sun <bangdasun94@gmail.com>
2017-07-18 22:04:50 -0400

    Update numpy-exercise.py

--- exercise/numpy-exercise.py
+++ exercise/numpy-exercise.py
@@ -119,7 +119,7 @@ x = np.pad(x, pad_width = 1, mode = 'constant', constant_values = 0)
 
 
 """
-Problem 16
+Problem 16X
 What is the result of the following expression
 0 * np.nan
 np.nan == np.nan
@@ -134,7 +134,7 @@ np.nan - np.nan
 0.3 == 3 * .1
 
 """
-Problem 17
+Problem 17X
 Create a 5x5 matrix with values 1,2,3,4 just below the diagonal
 """
 x = np.matrix(np.zeros((5, 5)))
@@ -146,7 +146,7 @@ x
 x = np.diag(1 + np.arange(4), k = -1)
 
 """
-Problem 18
+Problem 18X
 Create a 8x8 matrix and fill it with a checkerboard pattern
 """
 x = np.zeros((8, 8), dtype = int)
@@ -155,14 +155,14 @@ x[::2, 1::2] = 1
 x
 
 """
-Problem 19
+Problem 19X
 Consider a (6,7,8) shape array, what is the index (x,y,z) of the 100th element
 """
 np.unravel_index(100, (6, 7, 8))  # ???
 
 
 """
-Problem 20
+Problem 20X
 Create a checkerboard 8x8 matrix using the tile function
 """
 x = np.tile(np.array([[0, 1], [1, 0]]), (4, 4))
@@ -176,7 +176,7 @@ x = np.random.randint(20, size = (5, 5))
 (x - np.mean(x)) / np.std(x)
 
 """
-Problem 22
+Problem 22X
 Create a custom dtype that describes a color as four unsigned bytes (RGBA)
 """
 color = np.dtype([('r', np.ubyte, 1),
@@ -260,7 +260,7 @@ How to round away from zero a float array ?
 
 
 """
-Problem 29
+Problem 29X
 How to find common values between two arrays?
 """
 Z1 = np.random.randint(0,10,10)
@@ -271,6 +271,7 @@ print(np.intersect1d(Z1,Z2))
 Problem 30
 How to ignore all numpy warnings (not recommended)?
 """
+
 # -----------------------------------------------------------------------------
 """
 Problem 31
@@ -281,7 +282,7 @@ np.sqrt(-1)  # nan
 np.emath.sqrt(-1)  # 1j
 
 """
-Problem 32
+Problem 32X
 How to get the dates of yesterday, today and tomorrow?
 """
 today = np.datetime64('today', 'D')
@@ -327,7 +328,7 @@ x = np.zeros((5, 5))
 x += np.arange(5)
 
 """
-Problem 37
+Problem 37X
 Consider a generator function that generates 10 integers and use it to build an array
 """
 def generateInt(min, max):
@@ -355,7 +356,7 @@ x = np.random.randn(10)
 np.sort(x)
 
 """
-Problem 40
+Problem 40X
 How to sum a small array faster than np.sum?
 """
 x = np.arange(10)
@@ -363,7 +364,7 @@ np.add.reduce(x)
 
 # -----------------------------------------------------------------------------
 """
-Problem 41
+Problem 41X
 Consider two random array A and B, check if they are equal
 """
 A = np.random.randn(5)
@@ -373,7 +374,7 @@ np.sum(A == B) == 5
 np.array_equal(A, B)
 
 """
-Problem 42
+Problem 42X
 Make an array immutable (read-only)
 """
 z = np.zeros(10)
@@ -391,7 +392,7 @@ T = np.arctan2(Y, X)
 print(R, T)
 
 """
-Problem 44
+Problem 44X
 Create random vector of size 10 and replace the maximum value by 0
 """
 x = np.random.randn(10)
@@ -399,7 +400,7 @@ x[x == x.max()] = 0
 x[x.argmax()] = 0
 
 """
-Problem 45
+Problem 45X
 Create a structured array with x and y coordinates covering the [0,1]x[0,1] area
 """
 Z = np.zeros((5,5), [('x',float),('y',float)])
@@ -429,7 +430,6 @@ How to print all the values of an array?
 """
 
 
-
 """
 Problem 49
 How to find the closest value (to a given scalar) in a vector?
@@ -445,7 +445,7 @@ Create a structured array representing a position (x,y) and a color (r,g,b)
 
 # -----------------------------------------------------------------------------
 """
-Problem 51
+Problem 51X
 Consider a random vector with shape (100,2) representing coordinates, find point by point distances
 """
 X = np.random.randn(100, 2)
@@ -463,7 +463,7 @@ x = np.array([1, 2, 3], dtype = 'float32')
 x.astype('int32', copy = False)
 
 """
-Problem 53
+Problem 53X
 How to read the following file?
 1, 2, 3, 4, 5
 6,  ,  , 7, 8
@@ -477,7 +477,7 @@ Z = np.genfromtxt(s, delimiter=",", dtype=np.int)
 print(Z)
  
 """
-Problem 54 
+Problem 54X
 What is the equivalent of enumerate for numpy arrays?
 """
 # ndenumerate
@@ -493,7 +493,7 @@ sigma, mu = 1., 0.
 G = np.exp(- (D - mu)**2 / (2. * sigma ** 2))
 
 """
-Problem 56
+Problem 56X
 How to randomly place p elements in a 2D array?
 """
 p = np.array([1, 2, 3, 4])
@@ -515,7 +515,7 @@ X = np.matrix(np.array([[1, 2, 4],
 X - X.mean(axis = 1)  # 1 is horizontal, in R 1 is vertical
 
 """
-Problem 58
+Problem 58X
 How to sort an array by the nth column? 
 """
 X = np.array([[1, 2, 5],
@@ -524,7 +524,7 @@ X = np.array([[1, 2, 5],
 X[X[:, 1].argsort()]
     
 """
-Problem 59
+Problem 59X
 How to tell if a given 2D array has null columns? 
 """
 np.isnan(X)
@@ -539,3 +539,114 @@ X = np.random.randn(5, 3)
 X.flat[np.abs(X - element).argmin()]
 
 # X.flat --> expand X by row default
+# -----------------------------------------------------------------------------
+"""
+Problem 61
+Considering two arrays with shape (1,3) and (3,1), how to compute their sum using an iterator?
+"""
+
+
+"""
+Problem 62X
+Create an array class that has a name attribute 
+"""
+class array(object):
+    def __init__(self, name):
+        self.name = name;
+
+# solution
+class NamedArray(np.ndarray):
+    def __new__(cls, array, name="no name"):
+        obj = np.asarray(array).view(cls)
+        obj.name = name
+        return obj
+    def __array_finalize__(self, obj):
+        if obj is None: return
+        self.info = getattr(obj, 'name', "no name")
+
+Z = NamedArray(np.arange(10), "range_10")
+print (Z.name)
+
+"""
+Problem 63
+Consider a given vector, how to add 1 to each element indexed by a second vector (be careful with repeated indices)? 
+"""
+
+"""
+Problem 64
+How to accumulate elements of a vector (X) to an array (F) based on an index list (I)? 
+"""
+# use np.bincount()
+
+"""
+Problem 65
+Considering a (w,h,3) image of (dtype=ubyte), compute the number of unique colors 
+"""
+# use np.bincount()
+
+"""
+Problem 66X
+Considering a four dimensions array, how to get sum over the last two axis at once?
+"""
+X = np.random.randn(2, 2, 2, 2)
+X.sum(axis=(-2, -1))
+X.sum(axis=(2, 3))
+
+"""
+Problem 67
+Considering a one-dimensional vector D, how to compute means of subsets of D using a vector S of same size describing subset indices? 
+"""
+D = np.array([1, 3, 6, 3, 2, 5, 6])
+S = np.random.randint(0, len(D), size = len(D))
+D[S].mean()
+
+"""
+Problem 68
+How to get the diagonal of a dot product?
+"""
+X = np.matrix(np.array([[1, 0],
+                        [3, 5]]))
+Y = np.matrix(np.array([[1, 5],
+                        [6, 4]]))
+np.dot(X, Y).diagonal()
+
+# or use np.bincount()
+
+
+"""
+Problem 69
+Consider the vector [1, 2, 3, 4, 5], how to build a new vector with 3 consecutive zeros interleaved between each value? 
+"""
+x = [1, 2, 3, 4, 5]
+char = '000'.join(str(i) for i in x)
+lst = []
+for i in char:
+    lst.append(int(i))
+
+# or
+Z = np.array([1,2,3,4,5])
+nz = 3
+Z0 = np.zeros(len(Z) + (len(Z)-1)*(nz))
+Z0[::nz+1] = Z
+print(Z0)
+
+"""
+Problem 70 X
+71. Consider an array of dimension (5,5,3), how to mulitply it by an array with dimensions (5,5)?
+"""
+X = np.random.randn(5, 5, 3)
+Y = np.random.randn(5, 5)
+X * Y[:, :, None]
+
+"""
+Problem 71
+How to swap two rows of an array? 
+"""
+X = np.array([[1, 3, 4, 5],
+              [4, 2, 3, 1]])
+
+for i in range(X.shape[1]):
+    X[0, i], X[1, i] = X[1, i], X[0, i] 
+
+# or
+X[[1, 0], :] = X[[0, 1], :]
